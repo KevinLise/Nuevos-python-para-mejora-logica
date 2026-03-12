3d543cb96bf4bfd811d2b0745b7d65e5bd0a7a85
KevinLise <165961257+KevinLise@users.noreply.github.com>
2026-03-12 17:23:14 -0500

    Delete nuevas funciones pyhton directory

--- nuevas funciones pyhton/10.Academia de baile.py
+++
@@ -1,9 +0,0 @@
-amount = int(input("entre the amount of class :  "))
-if amount <= 5:
-    print("asitencia baja")
-elif amount <= 8:
-    print("asistencia media")
-elif amount >= 9:
-    print("asistencia alta")
-else:
-    ("no vas para el baile ")
--- nuevas funciones pyhton/pascal.py
+++
@@ -1,4 +0,0 @@
-n = int(input("ingrse numero:"))
-
-for i in range(1, n):
-    print(2**i)
